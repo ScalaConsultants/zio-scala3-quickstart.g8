b84074034d74c3d708582bee2ea1586b6a8837fb
ernestochero <ernestochr08@gmail.com>
2021-09-07 22:09:26 -0500

    graphql routes implementation

--- src/main/g8/src/main/scala/$package$/Main.scala
+++ src/main/g8/src/main/scala/$package$/Main.scala
@@ -1,11 +1,16 @@
 package $package$
 
+import caliban._
+import caliban.CalibanError.ValidationError
+import caliban.{ CalibanError, GraphQLInterpreter }
 import zhttp.http._
 import zhttp.service._
 import zhttp.service.server.ServerChannelFactory
 import zio._
 import zio.console._
 import zio.random._
+import zio.clock._
+import zio.blocking._
 import scala.util.Try
 import $package$.service._
 import $package$.repo._
@@ -24,12 +29,20 @@ object Main extends zio.App:
 
   def run(args: List[String]): URIO[ZEnv, ExitCode] =
     val nThreads: Int = args.headOption.flatMap(x => Try(x.toInt).toOption).getOrElse(0)
+    (for {
+      interpreter <- GraphqlApi.api.interpreter
+      _ <- server(interpreter)
+        .make
+        .use(_ => putStrLn(s"Server started on port \$port") *> ZIO.never)
+    } yield ()).provideCustomLayer(applicationLayer ++ EventLoopGroup.auto(nThreads)).exitCode
 
-    server
-      .make
-      .use(_ => console.putStrLn(s"Server started on port \$port") *> ZIO.never)
-      .provideCustomLayer(applicationLayer ++ EventLoopGroup.auto(nThreads))
-      .exitCode
+  def server( interpreter: GraphQLInterpreter[Console with Clock with Has[ItemService], CalibanError]
+    ): Server[Console with Blocking with Clock with Has[ItemService], Throwable] =
+    Server.port(port) ++
+      Server.app(
+        HealthCheck.healthCheck +++ HttpRoutes.app $if(add_websocket_endpoint.truthy)$ +++ WebSocketRoute.socketImpl $endif$ +++ GraphqlRoute
+          .route(interpreter)
+      )
 
   val server: Server[Console with Has[ItemService], Throwable] =
     Server.port(port) ++
---
+++ src/main/g8/src/main/scala/$package$/api/GraphqlRoute.scala
@@ -0,0 +1,27 @@
+package io.ernestochero.api
+
+import caliban._
+import caliban.CalibanError.ValidationError
+import caliban.{ CalibanError, GraphQLInterpreter, ZHttpAdapter }
+import io.ernestochero.service.ItemService
+import zio._
+import zio.stream._
+import zhttp.http._
+import zio.clock._
+import zio.console._
+import zio.blocking._
+
+object GraphqlRoute {
+  private val graphiql = Http.succeed(
+    Response.http(content = HttpData.fromStream(ZStream.fromResource("graphql/graphiql.html")))
+  )
+  def route(
+      interpreter: GraphQLInterpreter[Console with Clock with Has[
+        ItemService
+      ], CalibanError]
+    ): RHttpApp[Console with Blocking with Clock with Has[ItemService]] =
+    Http.route {
+      case _ -> Root / "api" / "graphql" => ZHttpAdapter.makeHttpService(interpreter)
+      case _ -> Root / "graphiql"        => graphiql
+    }
+}
