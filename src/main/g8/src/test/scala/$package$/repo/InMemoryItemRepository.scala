187963b0275046ebda6db84558af3521a47b256f
Eder Ruiz <edrzmr@gmail.com>
2022-11-25 11:40:13 -0300

    chore: Improve Ref variable name on InMemoryItemRepository

--- src/main/g8/src/test/scala/$package$/repo/InMemoryItemRepository.scala
+++ src/main/g8/src/test/scala/$package$/repo/InMemoryItemRepository.scala
@@ -5,41 +5,41 @@ import $package$.domain._
 
 final class InMemoryItemRepository(
     random: Random,
-    dataRef: Ref[Map[ItemId, Item]],
+    storeRef: Ref[Map[ItemId, Item]],
   ) extends ItemRepository:
 
   def add(description: String): IO[RepositoryError, ItemId] =
     for {
       itemId <- random.nextLong.map(_.abs)
       id      = ItemId(itemId)
-      _      <- dataRef.update(map => map + (id -> Item(id, description)))
+      _      <- storeRef.update(map => map + (id -> Item(id, description)))
     } yield id
 
   def delete(id: ItemId): IO[RepositoryError, Long] =
-    dataRef.modify { map =>
-      if (!map.contains(id)) (0L, map)
-      else (1L, map.removed(id))
+    storeRef.modify { store =>
+      if (!store.contains(id)) (0L, store)
+      else (1L, store.removed(id))
     }
 
   def getAll(): IO[RepositoryError, List[Item]] =
     for {
-      itemsMap <- dataRef.get
-    } yield itemsMap.view.values.toList
+      store <- storeRef.get
+    } yield store.view.values.toList
 
   def getById(id: ItemId): IO[RepositoryError, Option[Item]] =
     for {
-      values <- dataRef.get
-    } yield values.get(id)
+      store <- storeRef.get
+    } yield store.get(id)
 
   def update(item: Item): IO[RepositoryError, Option[Unit]] =
-    dataRef.modify { map =>
-      if (!map.contains(item.id)) (None, map)
-      else (Some(()), map.updated(item.id, item))
+    storeRef.modify { store =>
+      if (!store.contains(item.id)) (None, store)
+      else (Some(()), store.updated(item.id, item))
     }
 
 object InMemoryItemRepository:
   val layer: ZLayer[Random, Nothing, ItemRepository] =
     ZLayer(for {
-      random  <- ZIO.service[Random]
-      dataRef <- Ref.make(Map.empty[ItemId, Item])
-    } yield InMemoryItemRepository(random, dataRef))
+      random   <- ZIO.service[Random]
+      storeRef <- Ref.make(Map.empty[ItemId, Item])
+    } yield InMemoryItemRepository(random, storeRef))
