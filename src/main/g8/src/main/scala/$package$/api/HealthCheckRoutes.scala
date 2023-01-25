3b2ea8cc28639a03a54dd61360ee4daa67ee9611
Scala Steward <scala_steward@virtuslab.com>
2023-01-25 21:35:25 +0000

    Reformat with scalafmt 3.7.1
    Executed command: scalafmt --non-interactive

--- src/main/g8/src/main/scala/$package$/api/HealthCheckRoutes.scala
+++ src/main/g8/src/main/scala/$package$/api/HealthCheckRoutes.scala
@@ -9,14 +9,14 @@ object HealthCheckRoutes:
 
   val app: HttpApp[HealthCheckService, Nothing] = Http.collectZIO {
 
-  case Method.HEAD -> !! / "healthcheck" =>
-    ZIO.succeed {
-      Response.status(Status.NoContent)
-    }
+    case Method.HEAD -> !! / "healthcheck" =>
+      ZIO.succeed {
+        Response.status(Status.NoContent)
+      }
 
-  case Method.GET -> !! / "healthcheck" =>
-    HealthCheckService.check.map { dbStatus =>
-      if (dbStatus.status) Response.ok
-      else Response.status(Status.InternalServerError)
-    }
+    case Method.GET -> !! / "healthcheck" =>
+      HealthCheckService.check.map { dbStatus =>
+        if (dbStatus.status) Response.ok
+        else Response.status(Status.InternalServerError)
+      }
   }
